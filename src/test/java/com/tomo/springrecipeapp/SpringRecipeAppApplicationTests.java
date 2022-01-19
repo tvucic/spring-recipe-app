b0a84e1257f6136a82a202daf21dd05ddea8aa9d
Tomo <tomo.vucic@gmail.com>
2022-01-19 20:31:35 +0100

    Recipe app initial commit.

---
+++ src/test/java/com/tomo/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomo.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
